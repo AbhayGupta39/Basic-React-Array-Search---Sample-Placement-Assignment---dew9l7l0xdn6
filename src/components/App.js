2fe3c33e59480c9c187c25eac0d7f6a7bbaf602e
AbhayGupta39 <abhaygupta1400@gmail.com>
2022-10-06 10:48:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../styles/App.css';
 
 
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"];
+
+
 const App = () => {
 
-  
+  const [inputVal,setInputVal] = useState("");
+
+  let temp = searchArray.filter((el)=>{
+    if(inputVal !== "")
+    return el.toLowerCase().includes(inputVal.toLowerCase());
+  })
+  let result = temp.map((el)=>{
+    return <li>{el}</li>
+  })
   return (
+
     <div id="main">
+      <h4>Search</h4>
+      <input onChange={(event)=>{
+        setInputVal(event.target.value);
+      }} />
+      <h4>Results</h4>
+      <ul>
+        {result}
+      </ul>
       
     </div>
   )
